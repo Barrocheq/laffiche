ca9e739d288a6c34658340dc3ae895d4178021e1
Quentin Barroché <barrocheq@gmail.com>
2018-02-17 16:21:32 +0100

    add json

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -3,20 +3,18 @@
     <Toolbar ref="Toolbar" style="margin-bottom:72px"/>
     <h1 style="color:red">home</h1>
 
-  <div style="height: 500px; width: 500px; border: 1px solid red; position: relative;">
-    <vue-draggable-resizable :w="100" :h="100" v-on:dragging="onDrag" v-on:resizing="onResize" :parent="true">
-      <Annonce ref="annonce"/>
-    </vue-draggable-resizable>
-  </div>
-
-    
+    <div style="height: 500px; width: 500px; border: 1px solid red; position: relative;" >
+      <vue-draggable-resizable :w="100" :h="100" v-on:dragging="onDrag" v-on:resizing="onResize" :parent="true" v-for="card in data">
+        <Annonce ref="annonce" :card="card" />
+      </vue-draggable-resizable>
+    </div>
   </div>
 </template>
 
 <script>
 import Annonce from "@/components/Annonce";
 import Toolbar from "@/components/Toolbar";
-
+import json from "@/../static/data.json"
 import Vue from "vue";
 import VueDraggableResizable from "vue-draggable-resizable";
 
@@ -27,6 +25,7 @@ export default {
   name: "Home",
   data: function() {
     return {
+      data: json,
       width: 0,
       height: 0,
       x: 0,
@@ -45,12 +44,16 @@ export default {
       this.y = y;
     }
   },
-  mounted: function() {},
+  mounted: function() {
+    // console.log(this.data)
+  },
   components: {
     Annonce,
     Toolbar
   },
-  computed: {}
+  computed: {
+
+  }
 };
 </script>
 
