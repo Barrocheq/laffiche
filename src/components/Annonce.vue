2de5c87425d3ccda793aa2e56a7635c62aa62dd6
Quentin Barroché <barrocheq@gmail.com>
2018-02-17 14:55:01 +0100

    first design

--- src/components/Annonce.vue
+++ src/components/Annonce.vue
@@ -1,10 +1,30 @@
 <template>
-  <v-container fluid>
-    Annonce
-  </v-container>
+  <v-flex xs2>
+    <v-card color="blue-grey darken-2" class="white--text" style="width: 330px">
+      <v-card-title primary-title>
+		<!-- <v-icon large left color="white">insert_photo</v-icon> -->
+			<v-layout row wrap xs12 justify-align-content>
+				<v-layout>
+					<img src="../../static/img/avatar.svg" alt="" height="100">
+			        <v-layout row wrap xs12>
+						<div class="headline" xs12>BabySitting</div>
+						<div xs12>
+							<v-icon large left color="green">check_circle</v-icon>
+							/ 
+							<v-icon large left color="red">hourglass_empty</v-icon>
+						</div>
+			    	</v-layout>
+			    </v-layout>
+				<div>Bonjour, je suis Julie, j'ai 17 ans et je donne des cours de math au éléves de college jusqu'au Lycée. Si vous chercher quelqu'un de serieux pour vos enfants, n'hésitez pas à me contacter via l'Affiche.</div>
+		    </v-layout>
+      </v-card-title>
+      <v-card-actions>
+        <v-btn flat dark >Enregistrer</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-flex>
 </template>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
+<!-- <v-icon large color="green darken-2">check_circle</v-icon> -->
 <style scoped>
-
+	
 </style>
