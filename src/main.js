1d0e148bdcc2fdb9574e456cceef51b958480f74
Felix Becquart <felixbecquart1990@gmail.com>
2018-02-17 11:51:07 +0100

    first commit

---
+++ src/main.js
@@ -0,0 +1,29 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+import * as firebase from 'firebase'
+Vue.use(Vuetify)
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>',
+  created() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyDDgFX2BJVYWjEOIrcjl3vs0fh4hQ1EqwQ",
+      authDomain: "laffiche-18420.firebaseapp.com",
+      databaseURL: "https://laffiche-18420.firebaseio.com",
+      projectId: "laffiche-18420",
+      storageBucket: "",
+      messagingSenderId: "747026762550"
+    })
+  }
+})
